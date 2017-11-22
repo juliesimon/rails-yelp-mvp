6ca88bcf414187a909bf6ed23b92624d791f06d1
Julie Simon <juliesimon.js@gmail.com>
2017-11-22 15:25:27 +0100

    yelp work but not pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '+4156789676',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '+4156009698',
+  },
+  {
+    name:         'Chez Justine',
+    address:      'rue Oberkampf, 75011 Paris',
+    category:     'french',
+    phone_number: '+33153678907',
+  },
+  {
+    name:         'Les bonnes frites',
+    address:      'rue du Bac, 75007 Paris',
+    category:     'belgian',
+    phone_number: '+33153895300',
+  },
+  {
+    name:         'Super Sushi',
+    address:      'rue des Olympiades, 75013 Paris',
+    category:     'japanese',
+    phone_number: '+33153986791',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
